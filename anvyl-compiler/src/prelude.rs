89354ccaa4f711845e22c83153f0964e6c8494e3
Ben Lawrence <12blawrence4@gmail.com>
2025-06-07 18:05:14 +1200

    Create prelude

---
+++ anvyl-compiler/src/prelude.rs
@@ -0,0 +1,27 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub use crate::ast::evaluator::*;
+pub use crate::ast::lexer::*;
+pub use crate::ast::parser::*;
+
+pub use crate::diagnostics::*;
+pub use crate::text::*;
+
+pub use crate::ast::AST;
+
+pub use crate::ast::ASTStatement;
+pub use crate::ast::ASTStatementKind;
+
+pub use crate::ast::ASTExpression;
+pub use crate::ast::ASTExpressionKind;
+pub use crate::ast::ASTNumberExpression;
+pub use crate::ast::ASTParenthesizedExpression;
+
+pub use crate::ast::ASTBinaryExpression;
+pub use crate::ast::ASTBinaryOperatorKind;
+
+pub use crate::ast::ASTPrinter;
+pub use crate::ast::ASTVisitor;
+
+pub type DiagnosticsBagCell = Rc<RefCell<DiagnosticsBag>>;
