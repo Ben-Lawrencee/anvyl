fd636ed60392cf2227de55a5147b4de1dc90beda
Ben Lawrence <12blawrence4@gmail.com>
2025-06-29 18:33:06 +1200

    Update runner with undeclared variable example

--- anvyl-run/src/main.rs
+++ anvyl-run/src/main.rs
@@ -1,9 +1,13 @@
 use std::rc::Rc;
 
-use anvyl_compiler::{ast::evaluator::ASTEvaluator, prelude::*};
+use anvyl_compiler::{
+    ast::{evaluator::ASTEvaluator, symbols::SymbolChecker},
+    prelude::*,
+};
 
-fn main() {
-    let input = "let a = 10 + 11; let b = 20; let c = a * b;";
+fn main() -> Result<(), ()> {
+    let input = "let a = 10 + 11; let b = 20; let c = (a * b) + e;";
+    let text = SourceText::new(input);
 
     let mut lexer = Lexer::new(input);
     let mut tokens = Vec::new();
@@ -31,23 +35,30 @@ fn main() {
 
     ast.visualize();
 
-    let diagnostics_binding = diagnostics.borrow();
-    if !diagnostics_binding.is_empty() {
-        println!("");
-        println!("Diagnostics found:");
-        let source_text = SourceText::new(input);
-        let printer = Diagnostic::printer(&source_text, &diagnostics_binding.diagnostics);
+    check_diagnostics(&text, Rc::clone(&diagnostics))?;
 
-        for diagnostic in diagnostics_binding.diagnostics.iter() {
-            println!("{}", printer.stringify_diagnostic(diagnostic));
-        }
-        return;
-    }
+    let mut symbol_checker = SymbolChecker::new(Rc::clone(&diagnostics));
+    ast.visit(&mut symbol_checker);
 
-    println!("No diagnostics found, proceeding to evaluation...");
+    check_diagnostics(&text, Rc::clone(&diagnostics))?;
 
     let mut eval = ASTEvaluator::new();
     ast.visit(&mut eval);
 
     println!("Result: {:?}", eval.last_value);
+    Ok(())
+}
+
+fn check_diagnostics(text: &SourceText, diagnostics_bag: DiagnosticsBagCell) -> Result<(), ()> {
+    let diagnostics_binding = diagnostics_bag.borrow();
+    if !diagnostics_binding.is_empty() {
+        println!("");
+        println!("Diagnostics found:");
+        let printer = Diagnostic::printer(&text, &diagnostics_binding.diagnostics);
+
+        printer.print();
+        return Err(());
+    }
+
+    Ok(())
 }
